d4fc9f0452c5b7ade011a6e648568f91e04ef006
sachinjachak2001 <sachinjachak10@gmail.com>
2021-10-11 19:45:31 +0530

    setting redux environment

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '2d97469805mshf029bab47ab1addp1048acjsnee85893a1ec0'
+//     }
+//   };
